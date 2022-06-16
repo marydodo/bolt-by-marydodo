6f1e64135471568347ae67ba144d9e0824087c3d
Mary Do <mdo5128@gmail.com>
2022-06-16 14:42:51 -0500

    Update keymap.c

--- bolt/keymaps/marydodo/keymap.c
+++ bolt/keymaps/marydodo/keymap.c
@@ -15,12 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 
-#define LT2_TAB LT(2, KC_TAB)
-#define LT1_SPC LT(1, KC_SPC)
-#define CLIP_B LCTL(LSFT(KC_C))
-#define ALFRED_O LCTL(KC_SPC)
-#define QUES LSFT(KC_SLSH)
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Alpha Layer
@@ -48,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT(
 	       KC_GRV,   S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T),            S(KC_Y), S(KC_U), S(KC_I), S(KC_O),    S(KC_P),    KC_BSPC,
                KC_TAB,   S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G),            S(KC_H), S(KC_J), S(KC_K), S(KC_L),    S(KC_SCLN), KC_ENT,
-               ALFRED_O, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B),            S(QUES), S(KC_N), S(KC_M), S(KC_COMM), S(KC_DOT),  QUES, S(KC_ENT),
-               CLIP_B,   KC_LALT, KC_LGUI, KC_NO,                                S(KC_SCLN), KC_NO,                         KC_LEFT,    KC_DOWN,  KC_RGHT
+               KC_NO, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B),            S(QUES), S(KC_N), S(KC_M), S(KC_COMM), S(KC_DOT),  QUES, S(KC_ENT),
+               KC_LCTL,   KC_LALT, KC_LGUI, KC_NO,                                S(KC_SCLN), KC_NO,                         KC_LEFT,    KC_DOWN,  KC_RGHT
 	    ),
 
 };
