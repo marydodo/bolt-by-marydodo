8c3b489651d854ef5d58310d9620075cabee6a48
Mary Do <mdo5128@gmail.com>
2022-06-17 10:56:50 -0500

    Update keymap.c

--- marydodo/keymap.c
+++ marydodo/keymap.c
@@ -31,22 +31,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
                KC_GRV,   KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,              KC_CIRC,   KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
                KC_TILD,  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                 KC_UNDS,   KC_MINS,  KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-               KC_CAPS,    KC_6,    KC_7,   KC_8,    KC_9,   KC_0,                 QUES,   KC_BSPC,     KC_LCBR, KC_RCBR, KC_SLSH, QUES,  KC_NO,
-               CLIP_B,    ALFRED_O,                    KC_NO,  KC_NO,                KC_ENT,     KC_NO,                      KC_NO,   KC_NO, KC_NO
+               KC_CAPS,  KC_6,    KC_7,   KC_8,    KC_9,   KC_0,                 QUES,      KC_BSPC,  KC_LCBR, KC_RCBR, KC_SLSH, QUES,  KC_NO,
+               KC_LCTL,  KC_LALT,                  KC_NO,  KC_NO,                KC_ENT,    KC_NO,                      KC_NO,   KC_NO, KC_NO
              ),
 // FN, RESET, RGB config layer
     [2] = LAYOUT(
                KC_NO,    KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                KC_INS,  KC_DEL,   KC_HOME, KC_END,  KC_NO,   KC_NO,
-	             KC_NO,    KC_F6,   KC_F7,  KC_F8,   KC_F9,  KC_F10,               KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
-	             KC_NO,    KC_F11,  KC_F12, KC_NO,   KC_NO,  KC_NO,                RGB_TOG, RGB_RMOD, RGB_MOD, RGB_VAI, RGB_VAI, KC_NO, RESET,
-	             KC_NO,    KC_NO,                    KC_NO,  KC_NO,                KC_NO, KC_NO,                        KC_NO,   KC_NO, KC_NO
+	       KC_NO,    KC_F6,   KC_F7,  KC_F8,   KC_F9,  KC_F10,               KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+	       KC_NO,    KC_F11,  KC_F12, KC_NO,   KC_NO,  KC_NO,                RGB_TOG, RGB_RMOD, RGB_MOD, RGB_VAI, RGB_VAI, KC_NO, RESET,
+	       KC_NO,    KC_NO,                    KC_NO,  KC_NO,                KC_NO, KC_NO,                        KC_NO,   KC_NO, KC_NO
 	       ),
 // Shift Layer
     [3] = LAYOUT(
-	       KC_GRV,   S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T),            S(KC_Y), S(KC_U), S(KC_I), S(KC_O),    S(KC_P),    KC_BSPC,
-               KC_TAB,   S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G),            S(KC_H), S(KC_J), S(KC_K), S(KC_L),    S(KC_SCLN), KC_ENT,
-               KC_NO, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B),            S(QUES), S(KC_N), S(KC_M), S(KC_COMM), S(KC_DOT),  QUES, S(KC_ENT),
-               KC_LCTL,   KC_LALT, KC_LGUI, KC_NO,                                S(KC_SCLN), KC_NO,                         KC_LEFT,    KC_DOWN,  KC_RGHT
+	       KC_GRV,  S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T),            S(KC_Y), S(KC_U), S(KC_I), S(KC_O),    S(KC_P),    KC_BSPC,
+               KC_TAB,  S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G),            S(KC_H), S(KC_J), S(KC_K), S(KC_L),    S(KC_SCLN), KC_ENT,
+               KC_NO,   S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B),            S(QUES), S(KC_N), S(KC_M), S(KC_COMM), S(KC_DOT),  QUES,    S(KC_ENT),
+               KC_LCTL, KC_LALT, KC_LGUI, KC_NO,                                S(KC_SCLN), KC_NO,                     KC_LEFT,    KC_DOWN, KC_RGHT
 	    ),
 
 };
