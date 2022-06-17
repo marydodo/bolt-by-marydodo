765132eb7a9ca95f10eb91d83ee8444f3138c046
Mary Do <mdo5128@gmail.com>
2022-06-17 11:14:13 -0500

    Update keymap.c

--- marydodo/keymap.c
+++ marydodo/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       KC_GRV,  S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T),            S(KC_Y), S(KC_U), S(KC_I), S(KC_O),    S(KC_P),    KC_BSPC,
                KC_TAB,  S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G),            S(KC_H), S(KC_J), S(KC_K), S(KC_L),    S(KC_SCLN), KC_ENT,
                KC_NO,   S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B),            S(QUES), S(KC_N), S(KC_M), S(KC_COMM), S(KC_DOT),  QUES,    S(KC_ENT),
-               KC_LCTL, KC_LALT, KC_LGUI, KC_NO,                                S(KC_SCLN), KC_NO,                     KC_LEFT,    KC_DOWN, KC_RGHT
+               KC_LCTL, KC_LALT,                   KC_LGUI, KC_NO,              S(KC_SCLN), KC_NO,                     KC_LEFT,    KC_DOWN, KC_RGHT
 	    ),
 };
 
