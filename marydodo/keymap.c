7227112128d777b34a07f12482e1fb23bf0fff95
Mary Do <mdo5128@gmail.com>
2022-06-22 19:25:05 -0500

    Update keymap.c

--- marydodo/keymap.c
+++ marydodo/keymap.c
@@ -18,10 +18,6 @@
  */
 #include QMK_KEYBOARD_H
 
-#define CLIPB LCTL(LSFT(KC_C))
-#define ALFRED LCTL(KC_SPC)
-#define QUES LSFT(KC_SLSH)
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Alpha Layer
@@ -36,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_GRV,   KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC,              KC_CIRC,  KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
                KC_TILD,  KC_1,    KC_2,   KC_3,    KC_4,   KC_5,                 KC_UNDS,  KC_MINS,  KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                KC_CAPS,  KC_6,    KC_7,   KC_8,    KC_9,   KC_0,                 QUES,     KC_BSPC,  KC_LCBR, KC_RCBR, KC_SLSH, QUES,  KC_NO,
-               CLIPB,   ALFRED,                    KC_NO,  KC_NO,                KC_ENT,   KC_NO,                      KC_NO,   KC_NO, KC_NO
+               KC_LCTL,  KC_LALT,                  KC_NO,  KC_NO,                KC_ENT,   KC_NO,                      KC_NO,   KC_NO, KC_NO
              ),
 // FN, RESET, RGB config layer, gaming layer (tap toggled)
     [2] = LAYOUT(
-               KC_NO,    KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                KC_INS,  KC_DEL,   KC_HOME, KC_END,  KC_NO,   KC_NO,
-	       KC_NO,    KC_F6,   KC_F7,  KC_F8,   KC_F9,  KC_F10,               KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
-	       KC_NO,    KC_F11,  KC_F12, KC_NO,   KC_NO,  KC_NO,                RGB_TOG, RGB_RMOD, RGB_MOD, RGB_VAI, RGB_VAI, KC_NO, RESET,
-	       KC_NO,    KC_NO,                    KC_NO,  KC_NO,                KC_NO,   KC_NO,                        TT(4),   KC_NO, KC_NO
+               KC_NO,  KC_F1,   KC_F2,  KC_F3,   KC_F4,  KC_F5,                KC_INS,  KC_DEL,   KC_HOME, KC_END,  KC_NO,   KC_NO,
+	       KC_NO,  KC_F6,   KC_F7,  KC_F8,   KC_F9,  KC_F10,               KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+	       KC_NO,  KC_F11,  KC_F12, KC_NO,   KC_NO,  KC_NO,                RGB_TOG, RGB_RMOD, RGB_MOD, RGB_VAI, RGB_VAI, KC_NO, RESET,
+	       KC_NO,  KC_NO,                    KC_NO,  KC_NO,                KC_NO,   KC_NO,                      TT(4),   KC_NO, KC_NO
 	       ),
 // Shift Layer (only used for lighting layer) - can alternatively be used as an actual layer
     [3] = LAYOUT(
@@ -54,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	    ),
 // Gaming Toggle Layer (tap toggled)
     [4] = LAYOUT(
-                 KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-                 KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-                 KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_SLSH, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
-                 KC_LCTL,   KC_LALT,                   KC_LGUI, KC_SPC,             KC_SPC,  TT(4),                     KC_LEFT, KC_DOWN, KC_RGHT
+                 KC_ESC,  KC_Q,    KC_W,  KC_E,  KC_R,    KC_T,               KC_Y,    KC_U,  KC_I,  KC_O,    KC_P,    KC_BSPC,
+                 KC_TAB,  KC_A,    KC_S,  KC_D,  KC_F,    KC_G,               KC_H,    KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,
+                 KC_LSFT, KC_Z,    KC_X,  KC_C,  KC_V,    KC_B,               KC_SLSH, KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
+                 KC_LCTL, KC_LALT,               KC_LGUI, KC_SPC,             KC_SPC,  TT(4),                 KC_LEFT, KC_DOWN, KC_RGHT
                ),
 
 };
@@ -118,7 +114,7 @@ bool led_update_user(led_t led_state) {
     return true;
 }
 
-// Lights up the ligtning bolt using the shift_layer RGB_LAYER_SEGMENTS
+// Lights up the ligtning bolt YELLOW using the shift_layer RGB_LAYER_SEGMENTS
 bool process_record_user (uint16_t keycode, keyrecord_t *record) {
     // when shift is pressed or released, toggle the state of the lighting
     if (keycode == KC_LSFT) {
